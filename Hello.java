3637c3cc0d8596e098bb2b5bccfa85a9764d94e3
Tamilsankar <64898815+Tamilsankar@users.noreply.github.com>
2020-05-06 17:10:25 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
-public class Hello{
+public class Hello {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!!!!");
 
-public static void main(String[] args){
+	}
 
-  System.out.
+}
